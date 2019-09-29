7bb378b989fa03ea1feefe61852142e50502bd48
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-09-29 19:41:16 -0400

    deleted non-essential files

---
+++ js-test-app/src/components/css-test.jsx
@@ -0,0 +1,3 @@
+<div className='ex-copy'>
+
+</div>
---
+++ js-test-app/src/components/test.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+      <div className='test-component'>
+        <header>
+          <h1>Header</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Test;
